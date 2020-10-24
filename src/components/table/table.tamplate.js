f6d83a413f5544e4429e2fd8b3d21f3c767c5356
Levko Misha <misha_levko96@gmail.com>
2020-10-24 11:00:56 +0300

    finish base framework functional

---
+++ src/components/table/table.tamplate.js
@@ -0,0 +1,54 @@
+const CODES = {
+	A: 65,
+	Z: 90
+}
+
+// eslint-disable-next-line no-unused-vars
+function toCell() {
+	return `
+		<div class = "cell" contenteditable></div>
+	`
+}
+
+// eslint-disable-next-line no-unused-vars
+function toColumn(col) {
+	return `
+		<div class="column">${col}</div>
+	`
+}
+
+function createRow(index, content) {
+	return `
+		<div class="row">
+			<div class = "row-info">${index ? index : ''}</div>
+			<div class = "row-data">${content}</div>
+		</div>
+	`
+}
+
+function toChar(_, index) {
+	return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 15) {
+	// eslint-disable-next-line no-unused-vars
+	const colsCount = CODES.Z - CODES.A + 1
+	const rows = []
+	// eslint-disable-next-line no-unused-vars
+	const cols = new Array(colsCount)
+		.fill('')
+		.map(toChar)
+		.map(toColumn)
+		.join('')
+
+	rows.push(createRow(null, cols))
+	for (let i = 0; i < rowsCount; i++) {
+		const cells = new Array(colsCount)
+			.fill('')
+			.map(toCell)
+			.join('')
+		rows.push(createRow(i + 1, cells))
+	}
+
+	return rows.join('')
+}

1970e20f9b2c64f55e36b6493422cb10aceda001
Levko Misha <misha_levko96@gmail.com>
2020-11-07 09:09:30 +0200

    Finish redux and table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,18 @@
-import {ExcelComponent} from '@core/ExcelComponent'
+import {
+	ExcelComponent
+} from '@core/ExcelComponent'
+import {
+	$
+} from '@core/dom'
+import {
+	changeTitle
+} from '@/redux/actions'
+import {
+	defaultTitle
+} from '@/constants'
+import {
+	debounce
+} from '@core/utils'
 
 export class Header extends ExcelComponent {
 	static className = 'excel__header'
@@ -6,21 +20,37 @@ export class Header extends ExcelComponent {
 	constructor($root, options) {
 		super($root, {
 			name: 'Header',
-			...options
+			listeners: ['input'],
+			...options,
 		})
 	}
 
+	prepare() {
+		this.onInput = debounce(this.onInput, 300)
+	}
+
 	toHTML() {
+		const title = this.store.getState().title || defaultTitle
+		console.log(title);
 		return `
-			<input type = "text"class = "input" value = "Новая таблица"/>
-				<div class = "buttons-list">
-					<div class = "button">
-						<span class = "material-icons">delete </span>
-					</div>
-				<div class = "button">
-					<span class = "material-icons">exit_to_app </span> 
-				</div>
-			</div>
-		`
+      <input type="text" class="input" value="${title}" />
+
+      <div>
+
+        <div class="button">
+          <i class="material-icons">delete</i>
+        </div>
+
+        <div class="button">
+          <i class="material-icons">exit_to_app</i>
+        </div>
+
+      </div>
+    `
+	}
+
+	onInput(event) {
+		const $target = $(event.target)
+		this.$dispatch(changeTitle($target.text()))
 	}
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,12 @@
-/* eslint-disable space-before-blocks */
-/* eslint-disable no-unused-vars */
 import {ExcelComponent} from '@core/ExcelComponent'
 import {$} from '@core/dom'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
+import {isCell, matrix, nextSelection, shouldResize} from './table.functions'
 import {TableSelection} from '@/components/table/TableSelection'
-import {
-	isCell, matrix, nextSelection, shouldResize
-} from './table.functions'
-
+import * as actions from '@/redux/actions'
+import {defaultStyles} from '@/constants'
+import {parse} from '@core/parse'
 
 export class Table extends ExcelComponent {
 	static className = 'excel__table'
@@ -22,7 +20,7 @@ export class Table extends ExcelComponent {
 	}
 
 	toHTML() {
-		return createTable()
+		return createTable(20, this.store.getState())
 	}
 
 	prepare() {
@@ -34,55 +32,87 @@ export class Table extends ExcelComponent {
 
 		this.selectCell(this.$root.find('[data-id="0:0"]'))
 
-		this.$on('formula:input', text => {
-			this.selection.current.text(text)
+		this.$on('formula:input', value => {
+			this.selection.current.
+				attr('data-value', value)
+				.text(parse(value))
+			this.updateTextInStore(value)
 		})
 
 		this.$on('formula:done', () => {
 			this.selection.current.focus()
+		})
+
+		this.$on('toolbar:applyStyle', value => {
+			this.selection.applyStyle(value)
+			this.$dispatch(actions.applyStyle({
+				value,
+				ids: this.selection.selectedIds
+			}))
 		})
 	}
 
 	selectCell($cell) {
 		this.selection.select($cell)
 		this.$emit('table:select', $cell)
+		const styles = $cell.getStyles(Object.keys(defaultStyles))
+		this.$dispatch(actions.changeStyles(styles))
+	}
+
+	async resizeTable(event) {
+		try {
+			const data = await resizeHandler(this.$root, event)
+			this.$dispatch(actions.tableResize(data))
+		} catch (e) {
+			console.warn('Resize error', e.message)
+		}
 	}
 
 	onMousedown(event) {
 		if (shouldResize(event)) {
-			resizeHandler(this.$root, event)
+			this.resizeTable(event)
 		} else if (isCell(event)) {
 			const $target = $(event.target)
 			if (event.shiftKey) {
 				const $cells = matrix($target, this.selection.current)
-				.map(id => this.$root.find(`[data-id="${id}"]`))
+					.map(id => this.$root.find(`[data-id="${id}"]`))
 				this.selection.selectGroup($cells)
 			} else {
-				this.selection.select($target)
+				this.selectCell($target)
 			}
 		}
 	}
 
 	onKeydown(event) {
 		const keys = [
+			'Enter',
 			'Tab',
 			'ArrowLeft',
 			'ArrowRight',
 			'ArrowDown',
-			'ArrowUp',
-			'Enter'
+			'ArrowUp'
 		]
-		const {key} = event
+
+		const {
+			key
+		} = event
 
 		if (keys.includes(key) && !event.shiftKey) {
 			event.preventDefault()
 			const id = this.selection.current.id(true)
-			const $next =this.$root.find(nextSelection(key, id))
+			const $next = this.$root.find(nextSelection(key, id))
 			this.selectCell($next)
 		}
 	}
 
+	updateTextInStore(value) {
+		this.$dispatch(actions.changeText({
+			id: this.selection.current.id(),
+			value
+		}))
+	}
+
 	onInput(event) {
-		this.$emit('table:input', $(event.target))
+		this.updateTextInStore($(event.target).text())
 	}
 }
